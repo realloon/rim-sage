deaea64ef6c26cc7e6587eec06b3301aca0f111c
sincerajn <sincerajn@outlook.com>
2025-11-21 20:07:44 +0800

    chore: simplify config option

--- helper/db.ts
+++ helper/db.ts
@@ -1,10 +1,8 @@
 import { Database } from 'bun:sqlite'
 import * as sqliteVec from 'sqlite-vec'
-import { useLibSqlite3, libSqlite3Path, dbPath } from './config'
+import { libSqlite3Path, dbPath } from './config'
 
-if (useLibSqlite3) {
-  Database.setCustomSQLite(libSqlite3Path)
-}
+Database.setCustomSQLite(libSqlite3Path)
 
 export const db = new Database(dbPath)
 
